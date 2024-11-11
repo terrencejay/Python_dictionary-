65b316b0d0080b49177a25465705585524011eb8
terrencejay <150986847+terrencejay@users.noreply.github.com>
2024-11-10 18:54:33 -0600

    Create Customerservice.py

---
+++ Customerservice.py
@@ -0,0 +1,4 @@
+service_tickets = {
+    "Ticket001": {"Customer": "Alice", "Issue": "Login problem", "Status": "open"},
+    "Ticket002": {"Customer": "Bob", "Issue": "Payment issue", "Status": "closed"}
+}
